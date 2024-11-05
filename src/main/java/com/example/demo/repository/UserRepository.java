46de2024f9b9dbf98f4707bcf3776f65c2ff101b
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-05 14:34:46 -0300

    feat: add UserRepository for handling user database operations

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface AuthenticationRepository implements JpaRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByLogin(String login);
 }
