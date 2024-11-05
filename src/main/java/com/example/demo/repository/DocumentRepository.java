04f199ec6be77f998f2c331128b54173b9f1c05e
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-05 12:00:51 -0300

    feat: add DocumentRepository for handling documents database operations

--- src/main/java/com/example/demo/repository/DocumentRepository.java
+++ src/main/java/com/example/demo/repository/DocumentRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.repository;
 
-public class DocumentRepository {
+import com.example.demo.domain.document.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, UUID> {
 }
