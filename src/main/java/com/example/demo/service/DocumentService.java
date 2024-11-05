a96c433df534db2bcb268a34120f263480802c90
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-05 11:59:40 -0300

    feat: add DocumentService for handling documents business logics and rules

--- src/main/java/com/example/demo/service/DocumentService.java
+++ src/main/java/com/example/demo/service/DocumentService.java
@@ -1,4 +1,19 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.document.Document;
+import com.example.demo.repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DocumentService {
+
+    @Autowired
+    DocumentRepository repository;
+
+    public List<Document> listAllDocuments() {
+        return this.repository.findAll();
+    }
 }
