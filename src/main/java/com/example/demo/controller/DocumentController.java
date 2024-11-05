d417fff6e981887470dfa7f94eb106464f39959f
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-05 12:01:50 -0300

    feat: add DocumentController for handling documents crud operations

--- src/main/java/com/example/demo/controller/DocumentController.java
+++ src/main/java/com/example/demo/controller/DocumentController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.document.Document;
+import com.example.demo.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("documents")
 public class DocumentController {
+    @Autowired
+    DocumentService service;
+
+    @GetMapping
+    public ResponseEntity<List<Document>> getAllDocuments() {
+        return ResponseEntity.ok().body(this.service.listAllDocuments());
+    }
 }
