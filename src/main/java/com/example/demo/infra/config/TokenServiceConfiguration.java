eae0864102ff127300eeb6dfdf0708fb8e07f594
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-05 16:28:55 -0300

    feat: add TokenServiceConfiguration for generating, validating token and its expiration date

---
+++ src/main/java/com/example/demo/infra/config/TokenServiceConfiguration.java
@@ -0,0 +1,50 @@
+package com.example.demo.infra.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.demo.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenServiceConfiguration {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(generateExpirationDateTime())
+                    .sign(algorithm);
+        } catch (JWTCreationException ex) {
+            throw new RuntimeException("Error generating token", ex);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException ex) {
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDateTime() {
+        return LocalDateTime.now().toInstant(ZoneOffset.of("-03:00"));
+    };
+}
