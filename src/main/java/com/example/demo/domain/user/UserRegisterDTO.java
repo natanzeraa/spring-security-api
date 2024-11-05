81703932faa5a59957461555c905360d20d1bb95
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-05 14:33:27 -0300

    feat: add UserAuthenticationDTO for handling user register objects

--- src/main/java/com/example/demo/domain/user/UserRegisterDTO.java
+++ src/main/java/com/example/demo/domain/user/UserRegisterDTO.java
@@ -1,4 +1,4 @@
 package com.example.demo.domain.user;
 
-public record UserDTO(String login, String password, UserRole role) {
+public record UserRegisterDTO(String login, String password, UserRole role) {
 }
