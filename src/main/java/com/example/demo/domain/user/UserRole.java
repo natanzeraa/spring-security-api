a69fcc38bd6ae1cff6534c2c1d46871782ed8aee
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-06 17:00:40 -0300

    feat: add new UserRole VIEWER

--- src/main/java/com/example/demo/domain/user/UserRole.java
+++ src/main/java/com/example/demo/domain/user/UserRole.java
@@ -1,11 +1,17 @@
 package com.example.demo.domain.user;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
-    ADMIN("admin"), USER("user");
+    ADMIN("admin"),
+    VIEWER("viewer"),
+    USER("user");
 
     private String role;
 
-    UserRole(String role) {
+    UserRole(String role){
         this.role = role;
     }
+
 }
