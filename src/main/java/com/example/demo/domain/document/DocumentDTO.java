a9fd29c1f0f1c28903ce4aeeafada4b3f0f3f698
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-06 16:56:16 -0300

    refactor: changed user type from UUID to Integer

--- src/main/java/com/example/demo/domain/document/DocumentDTO.java
+++ src/main/java/com/example/demo/domain/document/DocumentDTO.java
@@ -1,6 +1,4 @@
 package com.example.demo.domain.document;
 
-import java.util.UUID;
-
-public record DocumentDTO(String title, String url, UUID user) {
+public record DocumentDTO(String title, String url, Integer user) {
 }
