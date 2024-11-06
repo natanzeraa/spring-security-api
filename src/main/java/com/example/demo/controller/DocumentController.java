6e77c10a631ebdbe72d31204f5d46a2b09f7e68d
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-06 16:55:42 -0300

    feat: add uploadDocument method for handling documents uploads business logic and rules

--- src/main/java/com/example/demo/controller/DocumentController.java
+++ src/main/java/com/example/demo/controller/DocumentController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.document.Document;
+import com.example.demo.domain.document.DocumentDTO;
 import com.example.demo.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,10 @@ public class DocumentController {
     @GetMapping
     public ResponseEntity<List<Document>> getAllDocuments() {
         return ResponseEntity.ok().body(this.service.listAllDocuments());
+    }
+
+    @PostMapping
+    public ResponseEntity<Document> uploadNewDocument(@RequestBody DocumentDTO data) throws Exception {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.uploadDocument(data));
     }
 }
