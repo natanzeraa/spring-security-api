6f482223272c9ce4d96bf9c6375d181368bfe266
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-06 16:56:52 -0300

    feat: add uploadDocument method for handling documents uploads business logic and rules

--- src/main/java/com/example/demo/service/DocumentService.java
+++ src/main/java/com/example/demo/service/DocumentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.document.Document;
+import com.example.demo.domain.document.DocumentDTO;
+import com.example.demo.domain.user.User;
 import com.example.demo.repository.DocumentRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +16,22 @@ public class DocumentService {
     @Autowired
     DocumentRepository repository;
 
+    @Autowired
+    UserRepository userRepository;
+
     public List<Document> listAllDocuments() {
         return this.repository.findAll();
+    }
+
+    public Document uploadDocument(DocumentDTO data) throws Exception {
+        User user = userRepository.findById(data.user())
+                .orElseThrow(() -> new Exception("User not found."));
+
+        Document newDoc = new Document();
+        newDoc.setTitle(data.title());
+        newDoc.setUser(user);
+        newDoc.setUrl(data.url());
+
+        return this.repository.save(newDoc);
     }
 }
